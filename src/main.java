221c29416957a623b66c89317a615c889232b468
Christopher Dolence <ecnelod@gmail.com>
2018-02-10 15:34:17 -0500

    Testing commit'ing via Eclipse. + inserted for loop as test.

--- src/main.java
+++ src/main.java
@@ -3,6 +3,11 @@ public class main {
 
 	public static void main(String[] args) {
 
+		int index;
+		
+		for(index = 0; index < 5; index++) {
+			System.out.println("index: " + index);
+		}
 		System.out.println("Hello World!");
 		
 	}// end main()
