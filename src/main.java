21a14b23907ecdad09520aa45c927915b4195516
Christopher Dolence <ecnelod@gmail.com>
2018-03-21 20:02:21 -0400

    Testing out a new IDE. Atom - https://atom.io/

--- src/main.java
+++ src/main.java
@@ -4,14 +4,14 @@ public class main {
 	public static void main(String[] args) {
 
 		int index;
-		
+
 		for(index = 0; index < 5; index++) {
 			System.out.println("index: " + index);
 		}
 		System.out.println("Hello World!");
 
 		// Jesse Test
-		System.out.println("Jesse Test");
+		System.out.println("Chris Test");
 		// another Jesse Test
 	}// end main()
 
